d06c1164faf9c1c9b6f7fbf2c27cd3dcb95cfc0f
Oscar Ayala <fabianayala675@gmail.com>
2025-11-18 20:20:50 -0300

    creacion el Modelo de Presupuesto

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -3,7 +3,10 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const db = new Sequelize( process.env.DATABASE_URL, {
-    
+    // todos los los archivos que esten dentro de la capeta modelo, sean tratados como modelo
+    models: [__dirname + '/../models/**/*'],
+    // no mostrara la creacion de las tablas por la terminal
+    logging:false,
     dialectOptions: {
         ssl: {
             require: false
---
+++ backend/src/models/Budget.ts
@@ -0,0 +1,20 @@
+import { Table, Column, DataType, HasMany, BelongsTo, ForeignKey, Model } from "sequelize-typescript"
+
+//Decorador
+@Table({
+    tableName: 'budgets'
+})
+
+class Budget extends Model {
+    @Column({
+        type: DataType.STRING(100)
+    })
+    name: string
+    
+    @Column({
+        type: DataType.DECIMAL
+    })
+    amount: number
+}
+
+export default Budget
