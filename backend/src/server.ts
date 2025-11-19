ed7f3d4a4ad3471aca0f3713d4916e5f753d7c37
Oscar Ayala <fabianayala675@gmail.com>
2025-11-18 21:00:46 -0300

    Creacion  de Router de Presupuesto API

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import colors from 'colors'
 import morgan from 'morgan'
 import { db } from './config/db'
+import budgetRouter from './routes/budgetRouter'
 
 
 async function connectDB(){
@@ -24,6 +25,7 @@ app.use(morgan('dev'))
 
 app.use(express.json())
 
+app.use('/api/budgets', budgetRouter)
 
 
 export default app
