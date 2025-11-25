237061b5c92396a45cf6a6691e009f04bbd3470a
Oscar Ayala <fabianayala675@gmail.com>
2025-11-25 14:53:22 -0300

    dependencias de express-validator cargamos paquetes, Agregando validaciones en las rutas a la creacion de presupuesto

--- backend/src/routes/budgetRouter.ts
+++ backend/src/routes/budgetRouter.ts
@@ -1,10 +1,23 @@
 import { Router } from 'express'
+import { body } from 'express-validator'
 import { BudgetController } from '../Controllers/BudgetController'
+import { handleInputErrors } from '../middleware/validation'
 
 const router = Router()
 
 router.get('/', BudgetController.getAll)
-router.post('/', BudgetController.create)
+
+router.post('/', 
+    body('name')
+        .notEmpty().withMessage('El nombre es obligatorio'),
+    body('amount')
+        .notEmpty().withMessage('El precio es obligatorio')
+        .isNumeric().withMessage('El precio es en numeros')
+        .custom(value => value > 0).withMessage('El valor tiene que ser mayor a 0'),
+    handleInputErrors,
+    BudgetController.create
+)
+
 router.get('/:id', BudgetController.getById)
 router.put('/:id', BudgetController.updateById)
 router.delete('/:id', BudgetController.deleteById)
